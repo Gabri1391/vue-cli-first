abab2d474d5dcfbbf95468509d78c6a398b0b8b5
Gabri1391 <chiarellogabriele@gmail.com>
2022-07-23 17:46:25 +0200

    BONUS

---
+++ src/components/MyHero.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="jumbo">
+        <img src="../assets/Mario.jpg" alt="Super Mario">
+        <div id="jumbo-text"> That's SuperMarioJumbo!</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'MyHero',
+};
+</script>
+
+<style scoped>
+  #jumbo{
+    height: 200px;
+    background-color: lightseagreen;
+    display: flex;
+    line-height: 200px;
+    color:white;
+    font-weight: 800;
+    font-size: 2rem;
+    margin: 0 50px;
+    border: 1px solid black;
+  };
+  
+</style>
