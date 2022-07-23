6ab4c9ed75a19f5d99adec78980e4e039e4613b8
Gabri1391 <chiarellogabriele@gmail.com>
2022-07-23 17:18:51 +0200

    exercise completed

---
+++ src/components/MyTitle.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+    <h1>My first app with Vue CLI!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'MyTitle',
+};
+</script>
+
+<style scoped>
+  h1{
+    color: red;
+  };
+</style>
